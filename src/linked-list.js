268c2253f7ddca11726d38b42a8ad86fdc79df32
v0vik <vovik_po@mail.ru>
2017-09-24 16:21:30 +0300

    10 pass, 4 fail

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,25 +1,79 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+        let node = new Node(data, this._tail, null);
+        
 
-    head() {}
+        if (this.length === 0) {
+            this._head = this._tail = node;
+        } else {
+            this._tail = this._tail.next = node;
+        }
 
-    tail() {}
+        this.length++;
+    }
 
-    at(index) {}
+    head() {
+        return this._head.data;
+    }
 
-    insertAt(index, data) {}
+    tail() {
+        return this._tail.data;
+    }
 
-    isEmpty() {}
+    at(index) {
+        let node = this._head;
+        while ( index > 0 ) {
+            node = node.next;
+            index--;
+        }
 
-    clear() {}
+        return node.data;
+    }
 
-    deleteAt(index) {}
+    insertAt(index, data) {
+        let node = this._head;
+        while ( index > 0 ) {
+            node = node.next;
+            index--;
+        }
 
-    reverse() {}
+        let insertedNode = new Node(data, node.prev, node);
+        node = node.prev.next = insertedNode;
+    }
+
+    isEmpty() {
+        return (this.length === 0);
+    }
+
+    clear() {
+        this.length = 0;
+        this._head = this._tail = new Node;
+    }
+
+    deleteAt(index) {
+        let node = this._head;
+        while ( index > 0 ) {
+            node = node.next;
+            index--;
+        }
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    reverse() {
+        let temp;
+        temp = this._head;
+        this._head = this._tail;
+        this._tail = temp;
+    }
 
     indexOf(data) {}
 }
